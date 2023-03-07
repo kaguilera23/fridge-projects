75d75910289f768f6f11b2786c6979092e42ded3
unknown <aguilera.k02@gmail.com>
2023-03-06 22:12:40 -0500

    added about me page, added to resume page

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -2,6 +2,19 @@ function Resume() {
     return (
         <div>
             <a href="./KaylaAguileraFullStackResume.pdf" download>Download My Resume Here</a>
+            <div>
+                <p>List of Developer Proficiencies</p>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                    <li>NodeJS</li>
+                    <li>ExpressJS</li>
+                    <li>MongoDB</li>
+                    <li>MySQL</li>
+                    <li>React</li>
+                </ul>
+            </div>
         </div>
     )
 };
